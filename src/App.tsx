9bdc7705851a457daf89a2b75c5b853e9bdd4232
stepan.rutz <stepan.rutz@gmx.de>
2025-03-31 21:45:13 +0200

    animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,52 @@
-import { ReactNode, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { MdArrowDropDown, MdArrowRight } from "react-icons/md";
+import { AnimatePresence, motion } from "motion/react";
+import { getWikipediaExcerpt, WikipediaExcerpt } from "./WikiExcerpt";
 
-function Box({ title = "hello", children } : { title?: string, children: ReactNode}) {
+function Box({ title = "hello", children }: { title?: string; children: ReactNode }) {
     const [open, setOpen] = useState(true)
     return (
         <div className="m-4 p-4 rounded-lg bg-white shadow-xl border border-gray-300 flex flex-col gap-4">
             <div className="flex gap-1 border-b border-gray-400">
-                <button onClick={() => { setOpen(!open) }}>
-                    {open 
-                        ? <MdArrowDropDown className="text-2xl"/> 
-                        : <MdArrowRight className="text-2xl"/>}
+                <button onClick={() => setOpen(!open)}>
+                    {open ? (
+                        <MdArrowDropDown className="text-2xl" />
+                    ) : (
+                        <MdArrowRight className="text-2xl" />
+                    )}
                 </button>
                 <Title>{title}</Title>
             </div>
-            {open && children}
+            <AnimatePresence>
+                {open && (
+                    <motion.div
+                        className="overflow-hidden flex flex-col gap-4"
+                        key="b1"
+                        initial={{ opacity: 0, height: 0 }}
+                        animate={{ opacity: 1, height: "auto" }}
+                        exit={{ opacity: 0, height: 0 }}
+                        transition={{ duration: 0.2 }}
+                    >
+                        {children}
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
-    )
+    );
 }
 
-function Title({children } : { children: ReactNode}) {
-    return (
-        <div className="font-bold">{children}</div>
-    )    
+function Title({ children }: { children: ReactNode }) {
+    return <div className="font-bold">{children}</div>;
 }
 
 export function App() {
     return (
-        <Box title="My Box 123">
-            <div>Lorem ipsum 123 hello again</div>
-        </Box>
+        <div className="w-[800px] flex flex-col">
+            <Box title="My Box 123">
+                <img className="self-center h-[300px]" src="/dom.jpg" alt="dom" />
+                <div className="text-sm text-gray-700">Lorem ipsum one two three.</div>
+            </Box>
+        </div>
     )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            '/api': {
+                target: 'https://en.wikipedia.org/w/api.php',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
+    }
 })
