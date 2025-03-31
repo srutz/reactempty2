250840bce73f77fe530ff0dc4516a456e8d9f4ba
stepan.rutz <stepan.rutz@gmx.de>
2025-03-31 15:11:41 +0200

    collapbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { ReactNode, useState } from "react";
+import { MdArrowDropDown, MdArrowRight } from "react-icons/md";
+
+function Box({ title = "hello", children } : { title?: string, children: ReactNode}) {
+    const [open, setOpen] = useState(true)
+    return (
+        <div className="m-4 p-4 rounded-lg bg-white shadow-xl border border-gray-300 flex flex-col gap-4">
+            <div className="flex gap-1 border-b border-gray-400">
+                <button onClick={() => { setOpen(!open) }}>
+                    {open 
+                        ? <MdArrowDropDown className="text-2xl"/> 
+                        : <MdArrowRight className="text-2xl"/>}
+                </button>
+                <Title>{title}</Title>
+            </div>
+            {open && children}
+        </div>
+    )
+}
+
+function Title({children } : { children: ReactNode}) {
+    return (
+        <div className="font-bold">{children}</div>
+    )    
+}
 
 export function App() {
     return (
-        <div className="text-4xl">Hello World</div>
+        <Box title="My Box 123">
+            <div>Lorem ipsum 123 hello again</div>
+        </Box>
     )
 }
 
