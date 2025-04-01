d6ac5d187999f8e50c5406ca31d30bc8c3d80c45
stepan.rutz <stepan.rutz@gmx.de>
2025-04-01 10:23:03 +0200

    more

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,72 @@
+import { useEffect, useState } from "react"
+
+
+
+export function useWindowSize() {
+    const [size, setSize] = useState({ 
+        width: window.innerWidth,
+        height: window.innerHeight
+    })
+    useEffect(() => {
+        console.log("mounting sizedisplay")
+        const l = () => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+        }
+        window.addEventListener("resize", l)
+        return () => {
+            console.log("unmounted sizedisplay")
+            window.removeEventListener("resize", l)
+        }
+    }, [])
+    return size
+}
+
+export function useFullscreen() {
+    const [isFullscreen, setIsFullscreen] = useState(false)
+    useEffect(() => {
+        const l = () => {
+            setIsFullscreen(document.fullscreenElement !== null)
+        }
+        document.addEventListener("fullscreenchange", l)
+        l()
+        return () => {
+            document.removeEventListener("fullscreenchange", l)
+        }
+    }
+    , [])
+    return isFullscreen
+}
+
+
+
+
+export function SizeDisplay() {
+    console.log("rerender")
+    const size = useWindowSize()
+    return (
+        <div id="x">{size.width} x {size.height}
+            {size.height > 400 && "HOCH"}</div>
+    )
+}
+
+export function Greeting({ title } : { title: string }) {
+    const size = useWindowSize()
+    return (
+        size.height > 400 
+            ? <div className="text-4xl">{title}</div>
+            : <div>{title}</div>
+    )
+}
 
 export function App() {
     return (
-        <div className="text-4xl">Hello World</div>
+        <div className="">
+            <Greeting title="Hello Hello" />
+            <SizeDisplay />
+        </div>
     )
 }
 
