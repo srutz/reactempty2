6e42aaaaf9a8c2584b6586e9774ff262c40d777f
stepan.rutz <stepan.rutz@gmx.de>
2025-04-01 13:11:30 +0200

    quotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,25 @@
 
+export type Quote = {
+    quote: string
+    author: string
+    id: number
+}
+
+export type QuotesResponse = {
+    quotes: Quote[]
+    total: number
+    skip: number
+    limit: number
+}
+
+export async function getQuotes(limit: number, skip: number) {
+    const response = await fetch("https://dummyjson.com/?limit=" 
+        + encodeURIComponent(limit)
+        + "&skip=" + encodeURIComponent(skip))
+    const data = await response.json() as QuotesResponse
+    return data
+}
+
 export function App() {
     return (
         <div className="text-4xl">Hello World</div>
