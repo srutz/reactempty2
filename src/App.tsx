1942d36d9f08b79d25dfd82a3f39876f642f507d
stepan.rutz <stepan.rutz@gmx.de>
2025-04-02 10:09:23 +0200

    routermenu

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { Link, NavLink, Outlet } from "react-router";
+
+export function Menubar() {
+    return (
+        <div className="p-2 bg-white border-b 
+            border-gray-400 flex gap-2 items-center">
+            <div className="select-none" >MY APP</div>
+            <div className="grow"></div>
+            <NavLink to="/">Home</NavLink> 
+            <NavLink to="/about">About</NavLink> 
+        </div>
+    )
+}
 
 export function App() {
     return (
-        <div className="text-4xl">Hello World</div>
+        <div className="flex-1 flex flex-col">
+            <Menubar/>
+            <Outlet></Outlet>
+        </div>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+function Home() {
+    return (
+        <div className="text-4xl 
+                grow flex flex-col justify-center items-center">
+            Home
+        </div>)
+}
+
+function About() {
+    return (
+        <div className="text-4xl 
+            grow flex flex-col justify-center items-center">
+            About
+        </div>)
+}
+
+const router = createBrowserRouter([
+    {
+        path: "/", element: <App/>, children: [
+            { path: "/", element: <Home></Home> },
+            { path: "/about", element: <About></About> },
+        ]
+    }
+])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 )
