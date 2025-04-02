7a77b2352b8dc3a4903d3d9b3aaf033d594062d4
stepan.rutz <stepan.rutz@gmx.de>
2025-04-02 14:22:34 +0200

    form

--- src/Form1.tsx
+++ src/Form1.tsx
@@ -1,39 +1,38 @@
 
 import { useState } from 'react'
 
-
-
 export function Form1() {
-
-    const [name, setName] = useState('')
-    const [firstName, setFirstName] = useState('')
-    const [email, setEmail] = useState('')
-
+    const [form,setForm] = useState({
+        lastName: "",
+        firstName: "",
+        email: "",
+    })
     const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault()
-        console.log(name, email)
+        e.preventDefault(); console.log(form)
     }
-
     return (
         <form onSubmit={handleSubmit} className="p-4 flex flex-col gap-2 items-start">
             <div className="grid grid-cols-2">
                 <label className="mr-2">Name:
                     <input
                         className="m-2"
-                        type="text" value={name} onChange={(e) => setName(e.target.value)}
+                        type="text" value={form.lastName} 
+                            onChange={(e) => setForm({ ...form, lastName: e.target.value})}
                     />
                 </label>
                 <label className="mr-2">Firstname:
                     <input
                         className="m-2"
                         type="text"
-                        value={firstName} onChange={(e) => setFirstName(e.target.value)}
+                        value={form.firstName} 
+                            onChange={(e) => setForm({ ...form, firstName: e.target.value})}
                     />
                 </label>
                 <label>Email:
                     <input
                         className="m-2"
-                        type="email" value={email} onChange={(e) => setEmail(e.target.value)}
+                        type="email" value={form.email} 
+                            onChange={(e) => setForm({...form, email: e.target.value})}
                     />
                 </label>
             </div>
