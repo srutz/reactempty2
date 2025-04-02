811da8352b6dc8c7489e0dd8f7e256d52c4b62c9
stepan.rutz <stepan.rutz@gmx.de>
2025-04-02 08:17:27 +0200

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { ReactNode } from "react"
+import { Outlet, useNavigate } from "react-router"
+
+export function Menuitem({ children, path } : { children: ReactNode, path: string}) {
+    const navigate = useNavigate()
+    return (
+        <button onClick={() => navigate(path)} className="hover:underline cursor-pointer">
+            {children}
+        </button>
+    )
+}
+
+export function Menubar() {
+    return (
+        <div className="h-[60px] px-4 py-1 bg-white border-b border-gray-400 items-center flex gap-2">
+            <div>ZZZ</div>
+            <div className="grow" />
+            <Menuitem path="/">Main</Menuitem>
+            <Menuitem path="/about">About</Menuitem>
+        </div>
+    )
+}
 
 export function App() {
     return (
-        <div className="text-4xl">Hello World</div>
+        <div className="grow flex flex-col">
+            <Menubar></Menubar>
+            <div className="h-1 grow flex">
+                <Outlet></Outlet>
+            </div>
+        </div>
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,34 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { App } from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+
+export function About() {
+    return (
+        <div className="grow flex flex-col justify-center items-center text-4xl">
+            About
+        </div>
+    )
+}
+
+export function Main() {
+    return (
+        <div className="grow flex flex-col justify-center items-center text-4xl">
+            Main
+        </div>
+    )
+}
+
+const router = createBrowserRouter([
+    { path: "/", element: <App></App>, children: [
+        { path: "/", element: <Main></Main> },
+        { path: "/about", element: <About></About> },
+    ] }
+])
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router}>
+        </RouterProvider>
     </StrictMode>,
 )
